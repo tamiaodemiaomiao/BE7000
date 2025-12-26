6fca32a8e68296fb0be6222c72e1b12e7443d430
tamiaodemiaomiao <cowboylyh@gmail.com>
2025-12-26 11:06:29 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -1,3 +1,8 @@
-# Modify Default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
-# sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+#!/bin/bash
+
+# 修改默认 IP 为 192.168.31.1 (小米默认网段)
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+
+# 添加第三方插件源
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
